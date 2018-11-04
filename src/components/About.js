e33d3ac804139fc8275cce3f6c3b6563abdb6810
duplijan <duplinszkijan@gmail.com>
2018-11-04 20:59:09 +0000

    gh-pages

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = (props) => {
   return(
     <div className='container'>
       <h1 className='center'>About</h1>
-      <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+      <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p> 
     </div>
   )
 }
